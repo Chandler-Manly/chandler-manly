caea900dddb1e58973a1834643b387887d79da22
HC <manlychandler@gmail.com>
2021-04-19 07:06:55 -0400

    updated landing page language

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import "../assets/Home.css";
 import Layout from "../components/shared/Layout";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <Layout>
       <div className="home">
-      <div className="welcome-message">
-        Salutations! I'm <div className="chandler">Chandler</div> and I am a Full Stack Software Engineer.
-      </div>
-      <Link to='/portfolio'><div className="view-portfolio">View Portfolio</div></Link>
+        <div className="welcome-message">
+          Salutations! I'm <div className="chandler">Chandler</div> and I am a
+          Full-Stack Software Engineer.
+        </div>
+
+        <Link to="/portfolio">
+          <div className="view-portfolio">View Portfolio</div>
+        </Link>
+        <div>
+          <h3>Finance, Entrepreneurship, and Technology</h3>
+          I recently completed General Assembly's Software Engineering Immersive
+          program. During this twelve week boot camp, I learned programming
+          languages and frameworks, computer science concepts, and agile
+          methodology. More importantly, I met and had the privilege to work
+          with some truly inspiring, saavy, and kind people. A few notable
+          highlights include:
+          <br />
+          <br />
+          <li>Drawing a blank during my first whiteboard challenge</li>
+          <li>Referring to Tom Brady when asked, "What's a MVP?"</li>
+          <li>
+            Sharing my favorite breakfast is an ultimate smoked Salmon bagel and
+            bottomless cortados
+          </li>
+        </div>
       </div>
     </Layout>
   );
